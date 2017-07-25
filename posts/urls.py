ac373057510a65d76ee86bf4905bc104c965073c
fawazkk <fawazkk@hotmail.com>
2017-07-25 18:47:24 +0300

    Create , Update , Delete ans some words

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
 
-    url(r'^create/',views.post_create,name='create'),
+    url(r'^list/$',views.post_list,name='list'),
+    url(r'^detail/(?P<post_id>\d+)/$',views.post_detail,name='detail'),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 
-def post_create(request):
-	post_list = Post.objects.all()
-	Post_kkk=Post.objects.all().first()
-	contest = {
-		'user': request.user,
-		'list': post_list,
-		'obj' : Post_kkk
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+
+def post_list(request):
+	obj_list = Post.objects.all()
+	context = {
+		"post_list": obj_list,
 	}
-	return render(request, 'create.html', context)
+	return render(request, 'post_list.html', context)
 	
+def post_detail(request, post_id):
+	obj = get_object_or_404(Post, id=post_id)
+	context = {
+		"instance": obj,
+		}
+
+	return render (request, 'post_detail.html', context)
